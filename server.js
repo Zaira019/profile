9375cc6b9c5ebd671aea381d44f3fd5529bb1b5c
Zaira019 <35891358+Zaira019@users.noreply.github.com>
2018-01-29 00:04:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ app.use(express.static((__dirname, 'shared')))
 
 
 app.get('/2', function(req, res) {
-
+	// ejs render automatically looks in the views folder
+	res.sendFile(path.join(__dirname + '/shared/index.html'));
+});
+app.get('/', function(req, res) {
 	// ejs render automatically looks in the views folder
 	res.sendFile(path.join(__dirname + '/shared/index0.html'));
 });
-
 
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
