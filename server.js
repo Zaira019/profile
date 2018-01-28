66cc7722e20231ae7db19e0677782b0b19cfe7d6
iamnodejs007 <iamnodejs007@gmaill.com>
2018-01-28 23:55:58 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express=require('express');
+var app = express();
+var path = require('path');
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static((__dirname, 'shared')))
+
+
+
+app.get('/2', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.sendFile(path.join(__dirname + '/shared/index0.html'));
+});
+
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
